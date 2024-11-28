80fb4c3f79aa54930eaa00b5fff2ebcd7d871e30
van4956 <gontscharowiwan@yandex.ru>
2024-11-28 09:42:32 +0800

    handler start

--- handlers/start.py
+++ handlers/start.py
@@ -25,7 +25,8 @@ async def start_cmd(message: Message, bot: Bot):
     user_name = message.from_user.username if message.from_user.username else 'None'
     user_id = message.from_user.id
     chat_id = bot.home_group[0]
-    await bot.send_message(chat_id=chat_id, text=f"✅ пользователь @{user_name} - запустил бота")
+    bot_username = bot.username
+    await bot.send_message(chat_id=chat_id, text=f"✅ пользователь @{user_name} - запустил бота @{bot_username}")
     await message.answer(text=(f'Привет {user_name}.\n\n'
                                 'Я персональный Telegram bot, model Т-5. '
                                 'Если ты не в списке администраторов, то твои команды не будут работать.\n\n'
